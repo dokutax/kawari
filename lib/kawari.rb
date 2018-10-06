fbafa3d14ed00eb99850b6974b48efd41bc4e566
dokutax <dokutax@gmail.com>
2018-10-06 01:26:01 -0500

    Refactored Logger & Downloader, added Rakefile

--- lib/kawari.rb
+++ lib/kawari.rb
@@ -1,9 +1,10 @@
 require 'dotenv/load'
-require 'kawari/version'
-require 'kawari/adapter'
-require 'kawari/helpers'
-require 'kawari/item'
-require 'kawari/models'
+require_relative 'kawari/version'
+require_relative 'kawari/adapter'
+require_relative 'kawari/download'
+require_relative 'kawari/item'
+require_relative 'kawari/logger'
+require_relative 'kawari/models'
 
 module Kawari
 
